09a8ea73a545a056e72523eba0a9dfe33dde667d
wh0isjack <allancgadelha@gmail.com>
2024-07-06 12:21:28 -0300

    feat: initial header structure | adjust config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
+import { Github } from 'lucide-react'
+// import React from 'react'
+
 export function App() {
 
   return (
-    <h1>
-      "Hello World"
-    </h1>
+    <div>
+      {/* https://tailwindcss.com/ */}
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-lg font-bold">viewer.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Developed with love by me with ❤️
+          </span>
+          <Separator orientation="vertical" className="h-6"/>
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2"/> 
+            Github 
+          </Button>
+        </div>
+
+      </div>
+    </div>
   )
 }
-
